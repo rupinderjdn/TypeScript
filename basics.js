72bf0a20b14293f73b8f453494f5fd0bc888426b
rupinderjdn <82199133+rupinderjdn@users.noreply.github.com>
2024-11-15 16:44:32 +0530

    basics of types

---
+++ basics.js
@@ -0,0 +1,9 @@
+var pifg = 3.1455676;
+console.log(Math.round(1234.456456));
+console.log("Hello");
+console.log("Hello" + 7);
+var movieTitle = "Amadeus";
+movieTitle = "Arrival";
+// movieTitle = 9;
+var numberList = 9;
+numberList += 9;
--- basics.ts
+++ basics.ts
@@ -1,1 +1,10 @@
-Math.round()
+var pifg: number = 3.1455676;
+console.log(Math.round(1234.456456))
+console.log("Hello")
+console.log("Hello" + 7)
+
+var movieTitle: string = "Amadeus";
+movieTitle = "Arrival";
+// movieTitle = 9;
+var numberList: number = 9;
+numberList +=9;
