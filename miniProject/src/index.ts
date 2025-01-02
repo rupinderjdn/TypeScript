490ec01ac63e37ae1177de3a10aedf9b4ae4872c
rupinderjdn <82199133+rupinderjdn@users.noreply.github.com>
2025-01-02 22:07:23 +0530

    typescript types for HTML elements

--- miniProject/src/index.ts
+++ miniProject/src/index.ts
@@ -1,1 +1,9 @@
 console.log("Hello World");
+
+const btn = document.getElementById("btn"); // type of HTMLElement
+// const btn = [] // this will give error as typescript knows that document is a poarticular object
+
+
+btn?.addEventListener("click", () => {
+  console.log("Button clicked");
+});
